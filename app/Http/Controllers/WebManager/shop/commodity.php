68e0192ec605e6b28611a16eb115e239de2cb958
york <show110686@gmail.com>
2017-10-26 17:20:53 +0800

    更新商品上傳

--- app/Http/Controllers/WebManager/shop/commodity.php
+++ app/Http/Controllers/WebManager/shop/commodity.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\WebManager\shop;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Services\web_judge_services;
+use App\Services\connection_services;
+
 class commodity extends Controller
 {
 	public $box;
@@ -38,9 +41,43 @@ class commodity extends Controller
 
     public function insert()
     {
-return back();
-    	dd($this->box);
-        return mMView('shop.insert');
+        //----------------------------------與廠商溝通----------------------------------
+        //放入連線區塊
+        //需呼叫的功能
+        $this->box->callFunction = 'AddCommodity';
+        $this->box->sendApiUrl = env('MANAGER_DOMAIN');
+
+        //放入資料區塊
+        $this->box->sendParams             = [];
+        $this->box->sendParams['title'] = $this->box->params->title;
+        $this->box->sendParams['subTitle']     = $this->box->params->subTitle;
+        $this->box->sendParams['imagesTitle']     = $this->box->params->imagesTitle;
+        $this->box->sendParams['imagesShow']     = $this->box->params->imagesShow;
+        $this->box->sendParams['menuID']     = $this->box->params->menuID;
+        $this->box->sendParams['price']     = $this->box->params->price;
+        $this->box->sendParams['points']     = $this->box->params->points;
+        $this->box->sendParams['transport']     = $this->box->params->transport;
+        $this->box->sendParams['quantity']     = $this->box->params->quantity;
+        $this->box->sendParams['style']     = $this->box->params->chstyle;
+        $this->box->sendParams['detail']     = $this->box->params->detail;
+        $this->box->sendParams['norm']     = $this->box->params->norm;
+        $this->box->sendParams['memo']     = $this->box->params->memo;
+
+        //送出資料
+        $this->box->result    = with(new connection_services())->callApi($this->box);
+        $this->box->getResult = $this->box->result;
+
+        //檢查廠商回傳資訊
+        $this->box = with(new web_judge_services($this->box))->check(['CAPI']);
+        if($this->box->status != 0){
+            return $this->reRrror(trans('message.error.'.$this->box->status));
+        }
+
+        //輸出成功訊息
+        setMesage([alert(trans('message.title.success'), trans('message.success.insertOK'), 1)]);
+
+        //重新導向
+        return redirect('/InsertShop');
     }
 
     public function reRrror($_msg)

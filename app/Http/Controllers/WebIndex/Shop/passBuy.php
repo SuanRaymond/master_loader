1e4fc781d377ad0d90422e784718ddad36d4b51f
Bin <snake0610@gmail.com>
2017-10-26 19:44:24 +0800

    修改 立即購買錯誤

--- app/Http/Controllers/WebIndex/Shop/passBuy.php
+++ app/Http/Controllers/WebIndex/Shop/passBuy.php
@@ -15,20 +15,46 @@ class passBuy extends Controller
         $this->box->result = (object) array();
         $this->box->params = (object) array();
 
-        $this->box->params->shopID        = Request()->get('ShopID', null);
+        $this->box->params->shopID  = Request()->get('ShopID', null);
+        $this->box->params->shopNum = 1;
     }
 
     public function index()
     {
-        $result = $this->search();
-        if(!$result){
-            return mIView('login');
-        }
+        // $result = $this->search();
+        // if(!$result){
+        //     return mIView('login');
+        // }
+        // if(empty(getSessionJson('SetShopID'))){
+        //     createSessionJson('SetShopID');
+        // }
+        // if(!searchSessionJson('SetShopID',$this->box->params->shopID)){
+        //     addSessionJson('SetShopID', $this->box->params->shopID);
+        // }
+        //判斷是否建立session
         if(empty(getSessionJson('SetShopID'))){
             createSessionJson('SetShopID');
         }
-        if(!searchSessionJson('SetShopID',$this->box->params->shopID)){
+        if(empty(getSessionJson('SetShopNum'))){
+            createSessionJson('SetShopNum');
+        }
+        //判斷是否存入session
+        if(!searchSessionJson('SetShopID', $this->box->params->shopID)){
             addSessionJson('SetShopID', $this->box->params->shopID);
+            addSessionJson('SetShopNum', $this->box->params->shopNum);
+        }
+        else{
+            $this->box->json = getSessionJson('SetShopID');
+            foreach($this->box->json as $key => $value){
+                if($this->box->params->shopID == $value){
+                    $this->box->jsonKey = $key;
+                }
+            }
+
+            $this->box->json = getSessionJson('SetShopNum');
+            $this->box->json[$this->box->jsonKey] = $this->box->json[$this->box->jsonKey] + 1;
+            removeSessionJson('SetShopNum');
+            session()->put('SetShopNum', json_encode($this->box->json));
         }
         $box = $this->box;
         return redirect('/ShopCar');
